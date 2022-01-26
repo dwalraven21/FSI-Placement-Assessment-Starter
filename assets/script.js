4bf0f322af94f2b1100a5e8190ef8b8f708bcfd6
Danielle <dwalraven21@gmail.com>
2022-01-25 19:59:12 -0500

    all event listeners working.

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,69 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Danielle Walraven" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-// selecting the element with an id of credit
+// selecting the elements
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+// Gingerbread
 const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+// Choc Chip
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+// Sugar Cookie
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+    console.log('Gingerbread + button was clicked!')
+
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Event listener for clicks on the "-" button for Gingerbread cookies
+gbMinusBtn.addEventListener('click', function() {
+
+    console.log('Gingerbread - button was clicked!')
+
+})
+
+// Event listener for clicks on the "+" button for Choc Chip cookies
+ccPlusBtn.addEventListener('click', function() {
+
+    console.log('Choc Chip + button was clicked!')
+
+})
+
+// Event listener for clicks on the "-" button for Choc Chip cookies
+ccMinusBtn.addEventListener('click', function() {
+
+    console.log('Choc Chip - button was clicked!')
+
+})
+
+// Event listener for clicks on the "+" button for sugar cookies
+sugarPlusBtn.addEventListener('click', function() {
+
+    console.log('sugar + button was clicked!')
+
+})
+
+// Event listener for clicks on the "-" button for sugar cookies
+sugarMinusBtn.addEventListener('click', function() {
+
+    console.log('sugar - button was clicked!')
+
+})
+
+
+    
